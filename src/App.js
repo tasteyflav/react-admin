ffde5d22b68b26d0ca78472ad7a5e826e83ee973
Mark Venema <markavenema@gmail.com>
2023-04-22 09:27:50 +0900

    stuff

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { useState } from 'react';
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Topbar from './scenes/global/Topbar';
-import Sidebar from './scenes/global/Sidebar/';
+import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard/';
 // import Team from './scenes/team/';
 // import Invoices from './scenes/invoices/';
@@ -17,14 +18,15 @@ import Dashboard from './scenes/dashboard/';
 
 function App() {
   const [theme, colorMode] = useMode();
-
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className='app'>
+          <Sidebar isSidebar={isSidebar} />
           <main className='content'>
-            <Topbar />
+            <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path='/' element={<Dashboard />} />
               {/* <Route path='/team' element={<Team />} /> */}
