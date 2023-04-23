0a38d84bebf82fecb0a7a40c1de7df567888d44d
tasteyflav <markavenema@gmail.com>
2023-04-23 18:53:43 +0900

    Finished

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -5,6 +5,7 @@ import ProgressCircle from './ProgressCircle';
 const StatBox = ({ title, subtitle, icon, progress, increase }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
+
   return (
     <Box width='100%' m='0 30px'>
       <Box display='flex' justifyContent='space-between'>
@@ -21,18 +22,18 @@ const StatBox = ({ title, subtitle, icon, progress, increase }) => {
         <Box>
           <ProgressCircle progress={progress} />
         </Box>
-        <Box display='flex' justifyContent='space-between'>
-          <Typography variant='h5' sx={{ color: colors.greenAccent[500] }}>
-            {subtitle}
-          </Typography>
-          <Typography
-            variant='h5'
-            fontStyle='italic'
-            sx={{ color: colors.greenAccent[600] }}
-          >
-            {increase}
-          </Typography>
-        </Box>
+      </Box>
+      <Box display='flex' justifyContent='space-between'>
+        <Typography variant='h5' sx={{ color: colors.greenAccent[500] }}>
+          {subtitle}
+        </Typography>
+        <Typography
+          variant='h5'
+          fontStyle='italic'
+          sx={{ color: colors.greenAccent[600] }}
+        >
+          {increase}
+        </Typography>
       </Box>
     </Box>
   );
